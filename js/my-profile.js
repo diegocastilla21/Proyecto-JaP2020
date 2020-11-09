6df690446ed7e9ab9bc253ad5d1f48d7e74c22cf
diegocastilla21 <fabian.castilla@outlook.es>
2020-11-08 21:33:06 -0300

    Entrega 7
    - Arreglo del LogIn
    - Arreglo en mi perfil de las flechas de los Input type number
    - Responsive de listado de productos
    - Un poco de mejoras en el carrito aunque aun no puedo sacar las flechas de los input type number de solo los formularios y no el de cantidad de productos

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,72 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+
+  let perfil = localStorage.getItem("perfil");
+
+  if (perfil) {
+    perfil = JSON.parse(perfil);
+
+    if (perfil.imgUrl != "") {
+
+      document.getElementById("imgPerfil").src = perfil.imgUrl;
+    }
+
+    document.getElementById("imgUrl").value = perfil.imgUrl;
+    document.getElementById("nombreUsuario").value = perfil.nombres;
+    document.getElementById("apellidoUsuario").value = perfil.apellidos;
+    document.getElementById("edadUsuario").value = perfil.edad;
+    document.getElementById("emailUsuario").value = perfil.email;
+    document.getElementById("telUsuario").value = perfil.telefono;
+  }
+
+  document
+    .getElementById("btnGuardarDatos").addEventListener("click", function (e) {
+      let passedValidation = true;
+      let imgUrl = document.getElementById("imgUrl");
+      let nombres = document.getElementById("nombreUsuario");
+      let apellidos = document.getElementById("apellidoUsuario");
+      let edad = document.getElementById("edadUsuario");
+      let email = document.getElementById("emailUsuario");
+      let telefono = document.getElementById("telUsuario");
+
+      if (nombres.value === "") {
+        nombres.classList.add("is-invalid");
+        passedValidation = false;
+      } else {
+        nombres.classList.remove("is-invalid");
+      }
+
+      if (apellidos.value === "") {
+        apellidos.classList.add("is-invalid");
+        passedValidation = false;
+      } else {
+        apellidos.classList.remove("is-invalid");
+      }
+
+      if (email.value === "") {
+        email.classList.add("is-invalid");
+        passedValidation = false;
+      } else {
+        email.classList.remove("is-invalid");
+      }
+
+      if (passedValidation) {
+        localStorage.setItem("perfil", JSON.stringify({
+
+            nombres: nombres.value,
+            apellidos: apellidos.value,
+            edad: edad.value,
+            imgUrl: imgUrl.value,
+            email: email.value,
+            telefono: telefono.value,
+
+          })
+
+        );
+
+        window.location = "my-profile.html";
+
+      }
+
+    });
 
 });
--- js/products.js
+++ js/products.js
@@ -11,133 +11,161 @@ var maxPre = undefined;
 
 //Funcion orden----------------------------------------------------------------------------
 function sortProducts(criterio, array) {
-    let result = [];
+  let result = [];
 
-    if (criterio === ORDER_ASC_BY_COST) {
-        result = array.sort(function (a, b) {
-            if (a.cost < b.cost) { return -1; }
-            if (a.cost > b.cost) { return 1; }
-            return 0;
-        });
-    } else if (criterio === ORDER_DESC_BY_COST) {
-        result = array.sort(function (a, b) {
-            if (a.cost > b.cost) { return -1; }
-            if (a.cost < b.cost) { return 1; }
-            return 0;
-        });
-
-    } else if (criterio === ORDER_DESC_BY_RELEVANCIA) {
-        result = array.sort(function (a, b) {
-            if (a.soldCount > b.soldCount) { return -1; }
-            if (a.soldCount < b.soldCount) { return 1; }
-            return 0;
-        });
-    }
-    return result;
+  if (criterio === ORDER_ASC_BY_COST) {
+    result = array.sort(function (a, b) {
+      if (a.cost < b.cost) {
+        return -1;
+      }
+      if (a.cost > b.cost) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (criterio === ORDER_DESC_BY_COST) {
+    result = array.sort(function (a, b) {
+      if (a.cost > b.cost) {
+        return -1;
+      }
+      if (a.cost < b.cost) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (criterio === ORDER_DESC_BY_RELEVANCIA) {
+    result = array.sort(function (a, b) {
+      if (a.soldCount > b.soldCount) {
+        return -1;
+      }
+      if (a.soldCount < b.soldCount) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  return result;
 }
-
 
 //Funcion listado productos----------------------------------------------------------------
 function showProductsList(array) {
+  let htmlContentToAppend = "";
+  for (let i = 0; i < array.length; i++) {
+    let product = array[i];
 
-    let htmlContentToAppend = "";
-    for (let i = 0; i < array.length; i++) {
-        let product = array[i];
-
-        if (((minPre == undefined) || (minPre != undefined && parseInt(product.cost) >= minPre)) &&
-            ((maxPre == undefined) || (maxPre != undefined && parseInt(product.cost) <= maxPre))) {
-
-            htmlContentToAppend += `
-            <a href="product-info.html" class="list-group-item list-group-item-action">
+    if (
+      (minPre == undefined ||
+        (minPre != undefined && parseInt(product.cost) >= minPre)) &&
+      (maxPre == undefined ||
+        (maxPre != undefined && parseInt(product.cost) <= maxPre))
+    ) {
+      htmlContentToAppend +=
+        `
+            <div class="col-lg-4 col-sm-12">
+            <div class="card my-3">
+            <div class="card-body">
+            <a href="product-info.html">
+            
         <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+            <div class="">
+                <div class="">
+                    <img src="` +
+        product.imgSrc +
+        `" alt="` +
+        product.description +
+        `" class="img-thumbnail">
                 </div>
-                <div class="col">
+                <div class="">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">` + product.name + " - " + product.currency + " " + product.cost + `</h4>
-                        <small class="text-muted">` + product.soldCount + `  vendidos</small>                      
+                        <h4 class="mb-1">` +
+        product.name +
+        " - " +
+        product.currency +
+        " " +
+        product.cost +
+        `</h4>
+                        <small class="text-muted">` +
+        product.soldCount +
+        `  vendidos</small>                      
                         </div>
-                    <p>` + product.description + `</p>                    
+                    <p>` +
+        product.description +
+        `</p>                    
                 </div>
             </div>
         </div>
         </a>
-        ` ;
-            }
-            document.getElementById("listaProductos").innerHTML = htmlContentToAppend;
-        }
+        </div>
+        </div>
+        </div>
+        `;
+    }
+    document.getElementById("listaProductos").innerHTML = htmlContentToAppend;
+  }
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      productsArray = resultObj.data;
+
+      //----------------------------------------------------
+      //Muestra por defecto ordenado por precios ascendentes
+      productsArray = sortProducts(ORDER_ASC_BY_COST, productsArray);
+      //----------------------------------------------------
+
+      showProductsList(productsArray);
+    }
+  });
+
+  // Funcionalidades de orden ascendente y descendente en función del precio y descendente en función de la relevancia
+  document.getElementById("costAsc").addEventListener("click", function (e) {
+    //Ordena por costo ascendente
+    productsArray = sortProducts(ORDER_ASC_BY_COST, productsArray);
+
+    //Muestra los prductos ordenados
+    showProductsList(productsArray);
+  });
+
+  document.getElementById("costDesc").addEventListener("click", function () {
+    //Ordena por costo descendente
+    productsArray = sortProducts(ORDER_DESC_BY_COST, productsArray);
+
+    //Muestra los productos ordenados
+    showProductsList(productsArray);
+  });
+
+  document
+    .getElementById("relevanciaDesdendente")
+    .addEventListener("click", function () {
+      //Ordena por relevancia descendente
+      productsArray = sortProducts(ORDER_DESC_BY_RELEVANCIA, productsArray);
+
+      //Muestra los productos ordenados
+      showProductsList(productsArray);
+    });
+
+  // Filtros a partir de rango de precio definido
+  document.getElementById("ir").addEventListener("click", function () {
+    minPre = document.getElementById("precio-min").value;
+    maxPre = document.getElementById("precio-max").value;
+
+    if (minPre != undefined && minPre != "" && parseInt(minPre) >= 0) {
+      minPre = parseInt(minPre);
+    } else {
+      minPre = undefined;
     }
 
+    if (maxPre != undefined && maxPre != "" && parseInt(maxPre) >= 0) {
+      maxPre = parseInt(maxPre);
+    } else {
+      maxPre = undefined;
+    }
 
-    //Función que se ejecuta una vez que se haya lanzado el evento de
-    //que el documento se encuentra cargado, es decir, se encuentran todos los
-    //elementos HTML presentes.
-
-    document.addEventListener("DOMContentLoaded", function (e) {
-
-        getJSONData(PRODUCTS_URL).then(function (resultObj) {
-            if (resultObj.status === "ok") {
-                productsArray = resultObj.data;
-
-                //----------------------------------------------------
-                //Muestra por defecto ordenado por precios ascendentes
-                productsArray = sortProducts(ORDER_ASC_BY_COST, productsArray);
-                //----------------------------------------------------
-
-                showProductsList(productsArray);
-            }
-        });
-
-
-        // Funcionalidades de orden ascendente y descendente en función del precio y descendente en función de la relevancia
-        document.getElementById("costAsc").addEventListener("click", function (e) {
-            //Ordena por costo ascendente
-            productsArray = sortProducts(ORDER_ASC_BY_COST, productsArray);
-
-            //Muestra los prductos ordenados
-            showProductsList(productsArray);
-        });
-
-        document.getElementById("costDesc").addEventListener("click", function () {
-            //Ordena por costo descendente
-            productsArray = sortProducts(ORDER_DESC_BY_COST, productsArray);
-
-            //Muestra los productos ordenados
-            showProductsList(productsArray);
-        });
-
-        document.getElementById("relevanciaDesdendente").addEventListener("click", function () {
-            //Ordena por relevancia descendente
-            productsArray = sortProducts(ORDER_DESC_BY_RELEVANCIA, productsArray);
-
-            //Muestra los productos ordenados
-            showProductsList(productsArray);
-        });
-
-
-        // Filtros a partir de rango de precio definido
-        document.getElementById("ir").addEventListener("click", function () {
-
-
-            minPre = document.getElementById("precio-min").value;
-            maxPre = document.getElementById("precio-max").value;
-
-            if ((minPre != undefined) && (minPre != "") && (parseInt(minPre)) >= 0) {
-                minPre = parseInt(minPre);
-            }
-            else {
-                minPre = undefined;
-            }
-
-            if ((maxPre != undefined) && (maxPre != "") && (parseInt(maxPre)) >= 0) {
-                maxPre = parseInt(maxPre);
-            }
-            else {
-                maxPre = undefined;
-            }
-
-            showProductsList(productsArray);
-        });
-    });
+    showProductsList(productsArray);
+  });
+});
